b25efc5e1447696270e95436dab4c28c2f4d1367
Chejohrpp <sergioroherp@gmail.com>
2023-10-24 23:32:14 -0600

    estructura del backend

---
+++ backend/src/parcel-management/parcel.js
@@ -0,0 +1,61 @@
+// parcel.js
+
+// Importa la conexión a la base de datos y otros módulos necesarios
+import db from './database/connection'; 
+
+// Función para ingresar un nuevo envío
+function ingresarEnvio(origen, destino, peso, volumen, tiempoMaximoEspera, callback) {
+    // Realiza la validación de datos
+    if (!origen || !destino || isNaN(peso) || isNaN(volumen) || isNaN(tiempoMaximoEspera)) {
+      return callback('Datos de envío no válidos', null);
+    }
+  
+    // Calcula la tarifa de precios en base a peso, volumen y tiempo máximo de espera
+    const costoEnvio = calcularTarifaPrecio(peso, volumen, tiempoMaximoEspera);
+  
+    // Genera un código único para el envío (puedes usar una biblioteca para esto)
+    const codigoUnico = generarCodigoUnico();
+  
+    // Realiza la inserción del envío en la base de datos
+    const sql = 'INSERT INTO envios (codigo, origen, destino, peso, volumen, costo) VALUES (?, ?, ?, ?, ?, ?)';
+    const values = [codigoUnico, origen, destino, peso, volumen, costoEnvio];
+  
+    db.query(sql, values, (error, result) => {
+      if (error) {
+        return callback('Error al ingresar el envío', null);
+      }
+      // Envío ingresado exitosamente, devuelve el código único
+      callback(null, codigoUnico);
+    });
+  }
+  
+  function calcularTarifaPrecio(peso, volumen, tiempoMaximoEspera) {
+    // Implementa la lógica para calcular la tarifa de precios aquí
+    // Este es solo un ejemplo simplificado
+    return (peso * 2) + (volumen * 1) + (tiempoMaximoEspera * 10);
+  }
+  
+  function generarCodigoUnico() {
+    // Implementa la generación de un código único aquí
+    // Puedes usar una biblioteca para generar UUID o implementar tu propia lógica
+    return 'C' + Math.random().toString(36).substring(2, 10).toUpperCase();
+  }
+  
+// Función para realizar seguimiento de un envío
+function realizarSeguimientoEnvio(codigoUnico, callback) {
+  // Realiza la lógica para obtener los detalles de un envío en base al código único
+  // Puede incluir la fecha de llegada y el costo del envío actualizados
+  // Llama al callback con el resultado
+}
+
+// Función para calcular la tarifa de precios
+function calcularTarifaPrecio(peso, volumen, tiempoMaximoEspera) {
+  // Realiza el cálculo de la tarifa de precios en base a los parámetros
+  // Devuelve el costo del envío
+}
+
+export default {
+  ingresarEnvio,
+  realizarSeguimientoEnvio,
+  calcularTarifaPrecio,
+};
